ae2d80516c51b16355446ce77e58e686af77d1d8
张少辉 <huis@ACA8010B.ipt.aol.com>
2016-10-14 18:44:53 +0800

    修改读取文件时可能存在死锁的问题

--- components/jsonParser.js
+++ components/jsonParser.js
@@ -2,41 +2,44 @@ var q = require('q');
 var fs = require('fs');
 var path = require('path');
 
+function readFiles(basePath, files) {
+    var promises = [];
+    for (var i = 0, n = files.length; i < n; i++) {
+        var file = files[i];
+        if (file.indexOf(".json") > -1) { //必须是.json的扩展文件
+            promises.push(q.Promise(function(resolve, reject, notify) {
+                fs.readFile(path.join(basePath, file), function(err, data) {
+                    if (err) {
+                        reject(err);
+                    }
+                    try {
+                        resolve(JSON.parse(data));
+                    } catch (e) {
+                        reject(e);
+                    }
+                })
+            }));
+        }
+    }
+    return q.all(promises);
+}
+
 //获取目录下面所有文件，并解析为json格式的数组
 var jsonParser = function(basePath) {
     var deferred = q.defer();
-    var resultData = [];
     try {
         fs.readdir(basePath, function(err, files) {
             if (err) {
-                console.error(err);
                 deferred.reject(err);
             }
             if (files.length < 1) {
-                deferred.resolve(resultData);
+                deferred.resolve([]);
             }
-            for (var i = 0, n = files.length; i < n; i++) {
-                var file = files[i];
-                var fileExtIndex = file.indexOf(".json");
-                if (fileExtIndex > -1) {
-                    fs.readFile(path.join(basePath, file), function(err, data) {
-                        if (err) {
-                            console.error(err);
-                            deferred.reject(err);
-                        }
-                        try {
-                            var jsonData = JSON.parse(data);
-                            resultData[resultData.length] = jsonData;
-                            if (i >= n - 1) {
-                                deferred.resolve(resultData);
-                            }
-                        } catch (e) {
-                            console.log(e);
-                            deferred.reject(e);
-                        }
-                    })
-                } 
-            }
+            readFiles(basePath, files).then(function(data) {
+                deferred.resolve(data)
+            }, function(error) {
+                deferred.reject(data)
+            })
         });
     } catch (e) {
         deferred.reject(e)
