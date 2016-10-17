74b0977970fa62b06d33f89904e382470eb51c05
张少辉 <huis@ACA80117.ipt.aol.com>
2016-10-17 13:44:59 +0800

    补充一点ReadMe文档

--- components/urlJsonParser.js
+++ components/urlJsonParser.js
@@ -15,4 +15,49 @@ var urlJsonParser = function() {
     }
 };
 
+function _getParentObj(allNameObj,partNames,parentName){
+    var parentObj = {
+        obj: null,
+        childs: allNameObj
+    };
+    for(var i=0,n=partNames.length;i<n;i++) {
+        var partName = partNames[i];
+        parentObj = parentObj.childs[partName];
+        if(partName == parentName) {
+            break;
+        }
+    }
+    return parentObj;
+}
+
+function parseUrlTree(data) {
+    var nameObj = {};
+    for (var i = 0, n = data.length; i < n; i++) {
+        var tmpUrlObj = data[i];
+        var statePartNames = tmpUrlObj.name.split(".");
+        if (statePartNames.length < 2) {
+            nameObj[tmpUrlObj.name] = {
+                obj: tmpUrlObj,
+                childs: {}
+            }
+        } else {
+            var j = statePartNames.length - 1;
+            var statePartName = statePartNames[j]; //printdetail
+            var parentName = statePartNames[j-1]; //printinfo
+            var parentObj = _getParentObj(nameObj, statePartNames,parentName)
+            if(parentObj) {
+                parentObj.childs[statePartName] = {
+                    obj: tmpUrlObj,
+                    childs: {}
+                }
+            } else {
+                console.log('no parent',parentName)
+            }
+        }
+    }
+    return nameObj;
+}
+
+urlJsonParser.parseUrlTree = parseUrlTree;
+
 module.exports = urlJsonParser;
--- routes/index.js
+++ routes/index.js
@@ -62,56 +62,13 @@ router.get('/jmxlists', function(req, res, next) {
     });
 });
 
-function getParentObj(allNameObj,partNames,parentName){
-    var parentObj = {
-        obj: null,
-        childs: allNameObj
-    };
-    for(var i=0,n=partNames.length;i<n;i++) {
-        var partName = partNames[i];
-        parentObj = parentObj.childs[partName];
-        if(partName == parentName) {
-            break;
-        }
-    }
-    return parentObj;
-}
-
-function parseUrlTree(data) {
-    var nameObj = {};
-    for (var i = 0, n = data.length; i < n; i++) {
-        var tmpUrlObj = data[i];
-        var statePartNames = tmpUrlObj.name.split(".");
-        if (statePartNames.length < 2) {
-            nameObj[tmpUrlObj.name] = {
-                obj: tmpUrlObj,
-                childs: {}
-            }
-        } else {
-            var j = statePartNames.length - 1;
-            var statePartName = statePartNames[j]; //printdetail
-            var parentName = statePartNames[j-1]; //printinfo
-            var parentObj = getParentObj(nameObj, statePartNames,parentName)
-            if(parentObj) {
-                parentObj.childs[statePartName] = {
-                    obj: tmpUrlObj,
-                    childs: {}
-                }
-            } else {
-                console.log('no parent',parentName)
-            }
-        }
-    }
-    return nameObj;
-}
-
 // show jmx lists page.
 router.get('/urllists', function(req, res, next) {
     urlJsonParser().then(function(data) {
         res.render('urllists', {
             title: "Url Lists",
             urllists: data[0],
-            classifyurllists: parseUrlTree(data[0]),
+            classifyurllists: urlJsonParser.parseUrlTree(data[0]),
             uploadUrl: "url",
             uploadTitle: "url(暂时不可用)"
         });
